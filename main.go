55218f4ab21ebd09f5587180803319ee72c0f8f1
ian-r274 <r.felix@buntulabs.com>
2024-11-25 15:52:38 +0300

    feat: can read all files from json

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 type Item struct {
-	Task   string
-	Status string
+	ID     int    `json:"id"`
+	Task   string `json:"task"`
+	Status string `json:"status"`
+}
+
+type Items struct {
+	Items []Item `json:"items"`
 }
 
 func main() {
+	todosJsonFile, err := os.Open("items.json")
 
-	todoItems := map[int]Item{}
-
-	fmt.Println("Todo items ")
-
-	for _, item := range todoItems {
-		fmt.Printf("%s\n", item)
+	if err != nil {
+		log.Fatalf("error in loading to do tasks %s", err)
 	}
+
+	byteValues, _ := io.ReadAll(todosJsonFile)
+
+	var todoItems Items
+
+	err = json.Unmarshal(byteValues, &todoItems)
+
+	if err != nil {
+		log.Fatalf("error in decoding json values from bytes %s", err)
+	}
+
+	fmt.Println("Todo items ", todoItems)
+
+	for _, item := range todoItems.Items {
+		fmt.Printf("Item %+v\n", item)
+	}
+
+	defer todosJsonFile.Close()
 }
